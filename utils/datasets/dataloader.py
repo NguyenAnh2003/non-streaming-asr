52f7e132be2dc79db4bb2318010b3954d1b26731
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-01 09:28:45 +0700

    #code

---
+++ utils/datasets/dataloader.py
@@ -0,0 +1,13 @@
+from torch.utils.data import DataLoader, Dataset
+
+""" define custom dataset """
+train_set = Dataset() #
+dev_set = Dataset()
+
+""" define data loader """
+train_dataloader = DataLoader()
+set_dataloader = DataLoader()
+
+""" check """
+if __name__ == "__main__":
+    pass
---
+++ utils/utils.py
@@ -0,0 +1,7 @@
+import yaml
+""" yaml load configs """
+
+def get_configs(path: str):
+    """ return defined configs in yaml file """
+    params = yaml.safe_load(open(path, 'r', encoding='utf-8'))
+    return params
