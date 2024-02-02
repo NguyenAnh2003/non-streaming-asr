206dcadf3d960aa55758456bad7cbb3478bf623c
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-02 15:15:18 +0700

    #code

--- feats_extraction/log_mel.py
+++ feats_extraction/log_mel.py
@@ -3,7 +3,25 @@ import torchaudio
 import torchaudio.functional as F
 import torchaudio.transforms as T
 
+""" Taking log mel spectrogram of audio """
 def audio_transforms(array, params):
-    """ Taking log mel spectrogram of audio """
-
-    return
+    """
+    :param array: audio array get by torchaudio
+    :param params: params config in yaml file
+    :return: log mel spectrogram
+    """
+    F_mel = T.MelSpectrogram(
+        sample_rate=params["sample_rate"],
+        n_fft=params["n_fft"],
+        win_length=params["win_length"],
+        hop_length=params["hop_length"],
+        window_fn=eval(params["window_fn"]),
+        center=params["center"],
+        pad_mode=params["pad_mode"],
+        power=params["power"],
+        norm=params["norm"],
+        n_mels=params["n_mels"],
+        mel_scale=params["mel_scale"],
+    )
+    mel_spectrogram = F_mel(array)
+    return mel_spectrogram
---
+++ utils/visualizer.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import torch
+import librosa
+
+""" visualize for audio and spectrogram """
+# Plot waveform
+def plot_waveform(waveform, sr, title="Waveform"):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sr
+
+    figure, axes = plt.subplots(num_channels, 1)
+    axes.plot(time_axis, waveform[0], linewidth=1)
+    axes.set_xlabel("time")
+    axes.set_ylabel("amplitude")
+    axes.grid(True)
+    figure.suptitle(title)
+    plt.show(block=False)
+
+# Plot Mel spectrogram
+def plot_melspectrogram(specgram, title=None, ylabel=None):
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title(title or "Mel Spectrogram")
+    axs.set_ylabel("Mel frequency")
+    axs.set_xlabel("Time")
+    im = axs.imshow(librosa.power_to_db(specgram), origin="lower", aspect="auto")
+    fig.colorbar(im, ax=axs)
+    plt.show(block=False)
