5210fe804ddf65d45270979d236c30d5e642b16f
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-06 08:50:06 +0700

    #code

--- data_loader/dataloader.py
+++ data_loader/dataloader.py
@@ -1,6 +1,5 @@
 from torch.utils.data import DataLoader, Dataset
 
-
 """ define custom dataset """
 train_set = Dataset() #
 dev_set = Dataset()
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,23 @@
+import time
+
 import yaml
 
 """ yaml load configs """
+
+
 def get_configs(path: str):
     """ return defined configs in yaml file """
     params = yaml.safe_load(open(path, 'r', encoding='utf-8'))
     return params
+
+def get_executing_time(start_time):
+    """
+    start time as param passing with time.time()
+    based on this start time to calculate the executing time of
+    a function
+    :param start_time
+    :return executing time
+    """
+    end_time = time.time() # calling current time
+    result = end_time - start_time # executing time
+    return result
