badcfd8afb4c5273991a27788c2df9440418250a
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-06 21:45:31 +0700

    #code

--- conformer/conformer_block.py
+++ conformer/conformer_block.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
+from modules import ResidualConnection
+from feed_forward import FeedForwardNet
+from convolution import ConvolutionModule
 
-""" Relative Positional Encoding """
 class RelativePositionalEncoding(nn.Module):
     def __init__(self):
         super(RelativePositionalEncoding, self).__init__()
@@ -9,28 +11,41 @@ class RelativePositionalEncoding(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return
 
-""" MHA with RPE """
 class MultiHeadedAttentionRPE(nn.Module):
-    def __init__(self):
+    def __init__(self, attention_heads: int = 4):
         super().__init__()
 
     def forward(self, x):
         return x
 
-""" Conformer Block """
 class ConformerBlock(nn.Module):
-    def __init__(self):
+    """
+    This is following Conformer architecture and Feed forward will follow Macaron Net
+    module have feed forward module -> half step = 0.5
+    multi head attention -> half step = 1
+    convolution module -> half step = 1
+    """
+    def __init__(self, half_step_residual: bool = True, attention_heads: int = 4):
         super().__init__()
         """ 1/2 Feed forward """
+        self.ff1 = ResidualConnection(module=FeedForwardNet(), residual_half_step=0.5)
 
         """ Multi-head Attention with RPE """
+        self.mha = ResidualConnection(module=MultiHeadedAttentionRPE(
+            attention_heads=attention_heads
+        ), residual_half_step=1.0)
 
         """ Convolution Module """
+        self.conv_module = ResidualConnection(module=ConvolutionModule(), residual_half_step=1.0)
 
         """ 1/2 Feed forward """
+        self.ff2 = ResidualConnection(module=ConvolutionModule(), residual_half_step=0.5)
 
         """ LayerNorm """
         self.layer_norm = nn.LayerNorm()
 
-    def forward(self, x):
-        return
+        """ Conformer block with LayerNorm and Chain """
+        self.conformer_block = nn.Sequential(self.ff1, self.mha, self.conv_module, self.ff2, self.layer_norm)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.conformer_block(x)
--- conformer/model.py
+++ conformer/model.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 from .convolution import SubsamplingConv
 from .conformer_block import ConformerBlock
 
+
 class Decoder(nn.Module):
-    def __init__(self):
-        self.lstm = nn.LSTM(bidirectional=True)
-        self.softmax = nn.Softmax()
+    def __init__(self, bidirectional: bool = True):
+        self.lstm = nn.LSTM(bidirectional=bidirectional) # suggest using MHA -> increase params
 
     def forward(self, x):
-        x = self.lstm(x)
-        return self.softmax(x) # perform soft max on output
+        return self.lstm(x) # perform soft max on output
+
 
 class Conformer(nn.Module):
     def __init__(self, dropout: float = 0.1):
         """ convolution subsampling """
-        self.conv_subsampling = SubsamplingConv() # config
+        self.conv_subsampling = SubsamplingConv()  # config
 
         """ linear """
         self.linear = nn.Linear()
@@ -24,13 +24,18 @@ class Conformer(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         """ conformer encoder """
-        self.conformer_encoder = ConformerBlock() # encoder
+        self.conformer_encoder = ConformerBlock()  # encoder
 
         """ decoder """
-        self.decoder = Decoder() #
+        self.decoder = Decoder(bidirectional=True)  #
+
+        """ softmax """
+        self.softmax = nn.Softmax()
 
         """ model chain """
-        self.chain = nn.Sequential(self.conv_subsampling, self.linear, self.dropout, self.conformer_encoder, self.decoder)
+        self.encoder_chain = nn.Sequential(self.conv_subsampling, self.linear, self.dropout, self.conformer_encoder)
 
     def forward(self, x):
-        return self.chain(x)
+        hidden_state = self.encoder_chain(x) # get relation ship between audio frame
+        output = self.decoder(hidden_state)
+        return self.softmax(output) # normalize output to probability with softmax
