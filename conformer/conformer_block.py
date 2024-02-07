a79a98a51207196123495917e5dff50776c17475
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-07 14:51:28 +0700

    #code

--- conformer/conformer_block.py
+++ conformer/conformer_block.py
@@ -1,17 +1,27 @@
 import torch
+from torch import Tensor
+from typing import Tuple, Optional
 import torch.nn as nn
 from torch.nn import MultiheadAttention
 from modules import ResidualConnection
 from feed_forward import FeedForwardNet
 from convolution import ConvolutionModule
 
-class MultiHeadedAttentionRPE(nn.Module):
-    def __init__(self, num_heads: int = 4):
-        super().__init__()
-        """ implement MHA with Relative Position """
+class RelativeMultiHeadedAttention(MultiheadAttention):
+    """ implement MHA with Relative Position """
 
-    def forward(self, x):
-        return x
+    def __init__(self, embed_dim, num_heads: int = 4):
+        """
+        :param embed_dim: embedding dimension (input is embedding)
+        :param num_heads: number of heads
+        """
+        super().__init__(embed_dim, num_heads)
+        """ u and v are trainable params proposed in Transformer-XL """
+        self.u = nn.Parameter()
+        self.v = nn.Parameter()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads # inherit number of heads
+
 
 class ConformerBlock(nn.Module):
     """
@@ -20,7 +30,8 @@ class ConformerBlock(nn.Module):
     multi head attention -> half step = 1
     convolution module -> half step = 1
     """
-    def __init__(self, d_model: int, dropout: float = 0.1, half_step_residual: bool = True, attention_heads: int = 4):
+
+    def __init__(self, dropout: float = 0.1, attention_heads: int = 4):
         super().__init__()
         """ 1/2 Feed forward """
         self.ff1 = ResidualConnection(module=FeedForwardNet(),
@@ -29,8 +40,7 @@ class ConformerBlock(nn.Module):
         """ Multi-head Attention with APE """
         self.mha = ResidualConnection(module=MultiheadAttention(
             num_heads=attention_heads,
-            dropout=dropout
-        ), residual_half_step=1.0)
+            dropout=dropout), residual_half_step=1.0)
 
         """ Convolution Module """
         self.conv_module = ResidualConnection(module=ConvolutionModule(),
@@ -49,6 +59,7 @@ class ConformerBlock(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.conformer_block(x)
 
+
 if __name__ == "__main__":
-    mha = MultiheadAttention(embed_dim=300, num_heads=4, dropout=0.1)
-    print(f"MHA : {mha}")
+    ff = ResidualConnection(module=FeedForwardNet(), residual_half_step=0.5)
+    print(f"Feed forward : {ff}")
--- conformer/feed_forward.py
+++ conformer/feed_forward.py
@@ -3,31 +3,29 @@ import torch.nn as nn
 from activations import Swish
 
 class FeedForwardNet(nn.Module):
-    def __init__(self, input_size: int, output_size: int, dropout: float):
+    def __init__(self, input_dim: int, hidden_dim: int, dropout: float = 0.1):
         super().__init__() # inherit Module
         """
         :param input_size: number of weight after dropout
         This FF network consists of LayerNorm -> Linear -> Dropout -> Linear -> Swish
         """
 
+        # LayerNorm explained: https://www.pinecone.io/learn/batch-layer-normalization/
         self.norm_layer = nn.LayerNorm() # config LayerNorm
 
         # config in feats and out feats of sub-linear 1 network
-        self.sub_linear1 = nn.Linear(in_features=input_size,
-                                     out_features=100, bias=True)
+        self.sub_linear1 = nn.Linear(in_features=input_dim,
+                                     out_features=hidden_dim, bias=True)
 
         # config dropout for common usage in FF block
         self.dropout = nn.Dropout(p=dropout)  # common dropout
 
         # config in feats and out feats of sub-linear 2 network
-        self.sub_linear2 = nn.Linear(in_features=100, out_features=10,
+        self.sub_linear2 = nn.Linear(in_features=hidden_dim, out_features=input_dim,
                                      bias=True)  # final Linear layer
 
         # Swish activation function
         self.swish = Swish()
-
-        # SiLU activation
-        self.silu = nn.SiLU()
 
         # combine all these block to form a sequence FF
         self.chain = nn.Sequential(
@@ -41,5 +39,7 @@ class FeedForwardNet(nn.Module):
 
     def forward(self, x):
         """ input is weights from Linear layer after Dropout """
-        x = self.chain(x)
-        return x  # return output of FF network
+        return self.chain(x)  # return output of FF network
+
+if __name__ == "__main__":
+    ff = FeedForwardNet()
