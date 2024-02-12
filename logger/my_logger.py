b97a53479c95201c503af82771126ac62cbc8334
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-12 23:19:14 +0700

    #code

---
+++ logger/my_logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+import time
+
+def setup_logger(name: str = "example"):
+    """ setup logger with logging package all log information will stored
+    in *.log files format date for logfile or filename """
+
+    # setup filename
+    filename = f"./{name}.log"
+
+    # format date logger
+    logging.Formatter(datefmt='%Y-%m-%d,%H:%M:%S',fmt='%(asctime)s.%(msecs)03d',)
+    # logger basic setup
+    logging.basicConfig(filename=filename, encoding='utf-8',
+                        level=logging.INFO, format='%(levelname)s:%(message)s')
+    # define logger for returning
+    logger = logging.getLogger(__name__)
+
+    return logger
+
+if __name__ == "__main__":
+    logger = setup_logger("example2")
+    logger.info(f"-- SETUP LOGGER --")
