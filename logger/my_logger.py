18f2fb9bf8c90fd2a19a1ecb4b5e373e439f1ae7
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-13 13:26:55 +0700

    #code

--- logger/my_logger.py
+++ logger/my_logger.py
@@ -1,4 +1,5 @@
 import logging
+from logging import LogRecord
 import os
 import time
 
@@ -8,7 +9,7 @@ def setup_logger(path: str = "./logs/example.log", location: str = None):
 
     # logger basic setup
     logging.basicConfig(filename=path, encoding='utf-8',
-                        level=logging.INFO,
+                        level=logging.DEBUG,
                         format=f'{location}: %(levelname)s: %(message)s: %(asctime)s',
                         datefmt="%m/%d/%Y %I:%M:%S %p")
     return logging
@@ -16,4 +17,7 @@ def setup_logger(path: str = "./logs/example.log", location: str = None):
 if __name__ == "__main__":
     logger = setup_logger(location="mylogger")
     logger.getLogger(__name__)
-    logger.info(f"-- SETUP LOGGER --")
+    logger.log(logger.INFO, "-- INFO LOGGER --")
+    logger.log(logger.DEBUG, "-- DEBUG LOGGER --")
+    logger.log(logger.WARN, "-- WARN LOGGER --")
+    logger.log(logger.CRITICAL, " -- CRITICAL LOGGER --")
