41cc78f90a10fd023c8f3fd3b117e55b3c31e9ef
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-18 01:15:19 +0700

    #code

--- data_manipulation/dataloader.py
+++ data_manipulation/dataloader.py
@@ -1,9 +1,10 @@
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, default_collate
 from feats_extraction.log_mel import audio_transforms
 from utils.utils import get_configs
 import torchaudio
 from logger.my_logger import setup_logger
 import pandas as pd
+import os
 
 logger = setup_logger(path="../logger/logs/dataset.log", location="dataloader")
 logger.getLogger(__name__)
@@ -11,31 +12,37 @@ logger.getLogger(__name__)
 # custom data_manipulation set
 class TrainSet(Dataset):
 
-    def __init__(self, csv_file, config_path: str = "../configs/audio_extraction.yaml"):
+    def __init__(self, csv_file, root_dir: str = "./", config_path: str = "../configs/audio_extraction.yaml"):
         """ define init """
         self.params = get_configs(config_path)  # defined params
-        self.train_csv = pd.read_csv(csv_file)
+        self.audio_samples = pd.read_csv(csv_file) # dataset defined as csv file
+        self.root_dir = root_dir # ./
 
     def __getitem__(self, index):
         logger.log(level=logger.DEBUG, msg="return log mel and transcript of each data_manipulation point")
         """ return log mel spectrogram, and transcript """
 
         # load audio to array and sample
-        sample_path = ""  # audio path for each sample index
+        sample_path = self._get_audio_sample_path(index)
+        audio_transcript = str(self.audio_samples.iloc[index, 1])
         array, rate = torchaudio.load(sample_path)
 
         # transform audio to mel spec
         log_mel = audio_transforms(array=array, params=self.params)
 
         # return log_mel and transcript
-        return log_mel
+        return log_mel, str(audio_transcript)
 
     def _get_audio_sample_path(self, index):
-        """ process audio path """
-        return
+        """ process audio path
+        :param index -> audio sample
+        :return path with audio sample .flac
+        """
+        sample_path = os.path.join(self.root_dir, self.audio_samples.iloc[index, 0])  # audio path for each sample index
+        return f"{sample_path}.flac"
 
     def __len__(self) -> int:
-        return
+        return len(self.audio_samples)
 
 
 class DevSet(Dataset):
@@ -68,4 +75,7 @@ class DevLoader(DataLoader):
 
 """ check """
 if __name__ == "__main__":
-    pass
+    train_set = TrainSet(csv_file="./train_samples.csv", root_dir="./librispeech/train-custom-clean")
+    data_loader = DataLoader(dataset=train_set, batch_size=1, shuffle=False, collate_fn=default_collate)
+    for step, (log_mel, transcript) in enumerate(data_loader):
+        print(f"Audio log mel: {log_mel.shape} Transcript: {type(transcript)}")
