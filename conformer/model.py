85b750e30fae9e9353dbb412c4a64140a75d0e39
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-03-16 15:35:28 +0700

    #code

--- conformer/model.py
+++ conformer/model.py
@@ -4,7 +4,7 @@ from convolution import ConvSubSampling
 from conformer_block import ConformerBlock
 import torchaudio.models.conformer
 
-class Decoder(nn.Module):
+class DecoderLSTM(nn.Module):
     def __init__(self, bidirectional: bool = True):
         super().__init__()
         self.lstm = nn.LSTM(bidirectional=bidirectional) # suggest using MHA -> increase params
@@ -16,29 +16,38 @@ class Decoder(nn.Module):
 class Conformer(nn.Module):
     """ Encoder Conformer """
 
-    def __init__(self, dropout: float = 0.1):
+    def __init__(self, in_channels: int, out_channels: int,
+                 kernel_size: int, stride: int, padding: int,
+                 dropout: float = 0.1):
         """ convolution subsampling """
         super().__init__()
-        self.conv_subsampling = ConvSubSampling()  # config
+        self.conv_subsampling = ConvSubSampling(in_channels=in_channels, out_channels=out_channels,
+                                                kernel_size=kernel_size, stride=stride, padding=padding)  # config
 
-        """ linear """
-        self.linear = nn.Linear()
+        self.flatten = nn.Flatten()
 
-        """ dropout """
+        # from conv to linear the feature must be flatten
+        self.linear = nn.Linear(in_features=out_channels)
+
+        # dropout
         self.dropout = nn.Dropout(p=dropout)
 
-        """ conformer encoder """
+        # conformer block - encoder
         self.conformer_encoder = ConformerBlock()  # encoder
 
-        """ decoder """
-        self.decoder = Decoder(bidirectional=True)  #
+        # lstm decoder
+        self.decoder = DecoderLSTM(bidirectional=True)  #
 
-        """ softmax """
+        # softmax
         self.softmax = nn.Softmax()
 
-        """ model chain """
-        self.encoder_chain = nn.Sequential(self.conv_subsampling, self.linear,
-                                           self.dropout, self.conformer_encoder)
+        # log-softmax
+        self.log_softmax = nn.LogSoftmax()
+
+        # model
+        self.encoder_chain = nn.Sequential(self.conv_subsampling, self.flatten,
+                                           self.linear, self.dropout,
+                                           self.conformer_encoder)
 
     def forward(self, x):
         hidden_state = self.encoder_chain(x) # get relation ship between audio frame
