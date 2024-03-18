3b353f06c5f4861d812ba4023532d8c40040c656
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-03-18 07:54:03 +0700

    #code

--- conformer/convolution.py
+++ conformer/convolution.py
@@ -18,11 +18,12 @@ class PointWise1DConv(nn.Module):
 class DepthWise1DConv(nn.Module):
     
     # audio just have 1 channel
-    def __init__(self, in_channels: int = 1, out_channels: int = 16, 
-                kernel_size: int = 3, stride: int = 1, padding: int = 1,
+    def __init__(self, in_channels: int , out_channels: int, 
+                kernel_size: int, stride: int = 1, padding: int = 1,
                 bias: bool = True):
         super(DepthWise1DConv, self).__init__()
-        self.dw_conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, 
+        # depth wise -> groups
+        self.dw_conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
                                 stride=stride, padding=padding, bias=bias, groups=in_channels)
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
