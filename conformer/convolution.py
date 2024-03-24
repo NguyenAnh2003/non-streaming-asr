034fb7864fe6e22322a23588350e5e7313d354da
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-03-24 13:48:27 +0700

    #code

--- conformer/convolution.py
+++ conformer/convolution.py
@@ -4,7 +4,7 @@ from activations import Swish
 
 class PointWise1DConv(nn.Module):
     def __init__(self, in_channels: int = 0, out_channels: int = 1, 
-                kernel_size: int = 3, stride: int = 1, padding: int = 1,
+                kernel_size: int = 1, stride: int = 1, padding: int = 1,
                 bias: bool = True):
         """ point wise conv """
         super(PointWise1DConv, self).__init__()
@@ -19,12 +19,12 @@ class DepthWise1DConv(nn.Module):
     
     # audio just have 1 channel
     def __init__(self, in_channels: int , out_channels: int, 
-                kernel_size: int, stride: int = 1, padding: int = 1,
+                kernel_size: int = 1, stride: int = 1, padding: int = 1,
                 bias: bool = True):
         super(DepthWise1DConv, self).__init__()
         # depth wise -> groups
         self.dw_conv = nn.Conv1d(in_channels=in_channels, groups=in_channels, 
-                                out_channels=out_channels, kernel_size=kernel_size,
+                                out_channels=in_channels, kernel_size=kernel_size,
                                 stride=stride, padding=padding, bias=bias)
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
@@ -36,6 +36,7 @@ class ConvSubSampling(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, 
                 kernel_size: int = 3, stride: int = 2, padding: int = 0):
         super(ConvSubSampling, self).__init__()
+        # stride = 2 -> expirement: using max pooling layer
         self.chain = nn.Sequential(
             nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
                       kernel_size=kernel_size, stride=stride, padding=padding),
@@ -52,27 +53,29 @@ class ConvSubSampling(nn.Module):
 
 class ConvolutionModule(nn.Module):
     """ implemented Conv module sequentially """
-    def __init__(self, in_channels: int, out_channels: int,
+    def __init__(self, in_channels: int, out_channels: int, hidden_units: int,
                  stride: int, padding: int, bias: bool):
         super().__init__()
 
         self.norm_layer = nn.LayerNorm() # normalize with LayerNorm
 
-        self.point_wise1 = PointWise1DConv(in_channels=in_channels, stride=stride,
-                                           padding=padding, bias=bias) # customized Pointwise Conv
+        self.point_wise1 = PointWise1DConv(in_channels=in_channels, out_channels=out_channels, kernel_size=1, 
+                                           stride=stride, padding=padding, bias=bias) # customized Pointwise Conv
 
         self.glu_activation = nn.GLU()
 
         """ Depthwise Conv 1D """
-        self.dw_conv = DepthWise1DConv()
+        self.dw_conv = DepthWise1DConv(in_channels=out_channels, out_channels=out_channels, 
+                                       kernel_size=1, padding=padding, bias=bias)
 
         """ this batch norm layer stand behind the depth wise conv (1D) """
-        self.batch_norm = nn.BatchNorm1d()
+        self.batch_norm = nn.BatchNorm1d(num_features=out_channels)
 
         """ Swish activation """
         self.swish = Swish()
 
-        self.point_wise2 = PointWise1DConv() #
+        self.point_wise2 = PointWise1DConv(in_channels=out_channels, out_channels=out_channels, kernel_size=1,
+                                           stride=1, padding=0, bias=True) #
 
         self.dropout = nn.Dropout(p=0.1)
 
@@ -101,6 +104,9 @@ if __name__ == "__main__":
     print(f"Shape: {subsampling(x).shape}")
 
     # depth wise 1D
+    a = torch.randn(16, 1, 81)
+    dw = DepthWise1DConv(in_channels=1, out_channels=1)
+    print(f"Depthwise: {dw(a).shape}")
     
     # point wise 1D
 
