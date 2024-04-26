a9e74b413f72f3ed16347bff88cda0ddcbd5d1b0
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-04-26 15:10:57 +0700

    #code

--- finetuning_recipe/finetuning_conformer_large.py
+++ finetuning_recipe/finetuning_conformer_large.py
@@ -1,2 +1,36 @@
-path = "../data_manipulation/librispeech/train-custom-clean"
+import pytorch_lightning as pl
+from omegaconf import DictConfig
+import nemo.collections.asr as nemo_asr
+from utils.utils import get_configs
+
+def main(MODEL_NAME: str, params):
+  # get pretrained model
+  conformer_large = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name=MODEL_NAME)
+
+  # optimizer
+
+  # dataloader
+  conformer_large.setup_training_data(train_data_config=params['model']['train_ds'])
+  conformer_large.setup_validation_data(val_data_config=params['model']['validation_ds'])
+
+  trainer = pl.Trainer(accelerator="gpu", max_epochs=50)
+  trainer.fit(conformer_large)
+
+
+
+if __name__ == "__main__":
+  path = "../data_manipulation/librispeech/train-custom-clean"
+  params = get_configs("../configs/conformer_ctc_bpe.yaml")
+  MODEL_LARGE = "nvidia/stt_en_conformer_ctc_large"
+
+  # dataloader
+  params['model']['train_ds']['manifest_filepath'] = "../data_manipulation/"
+  params['model']['validation_ds']['manifest_filepath'] = "../data_manipulation/"
+  params['model']['test_ds']['manifest_filepath'] = "../data_manipulation/"
+
+  main(MODEL_NAME=MODEL_LARGE, params=params)
+
+  print(params)
+
+
 
