9219a09fe53d63e0e97d10c2dd987f6a27089a15
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-05-30 00:27:39 +0700

    #code

--- models/asr_model.py
+++ models/asr_model.py
@@ -4,30 +4,32 @@ import torch.nn as nn
 import torch
 from torch import Tensor
 from typing import List
+from functools import cache
 
-def get_pretrained_model(model_name):
-    asr_model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name)
-    return asr_model
 
 
 class ASRModel(nn.Module):
-    def __init__(self, encoder):
+    def __init__(self, pretrained_name):
         super().__init__()
-        self.encoder = encoder
         self.linear = nn.Linear(100, 300, bias=True)
+        self.encoder = self.get_pretrained_encoder(pretrained_name)
 
         self.chain = nn.Sequential(self.encoder, self.linear)
+
+    @cache
+    def get_pretrained_encoder(model_name):
+        asr_model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name)
+        return (asr_model, asr_model.encoder)
 
     def forward(self, x: Tensor, lengths: List):
         out = self.encoder(x)
         return out
+
 def main():
     params = get_configs("../configs/asr_model_ctc_bpe.yaml")
 
     pretrained_model_name = "nvidia/stt_en_conformer_ctc_large"
-    pretrained_model = get_pretrained_model(pretrained_model_name)
-    p_encoder = pretrained_model.encoder # init pretrained encoder
-    asr_model = ASRModel(p_encoder)
+    asr_model = ASRModel(pretrained_name=pretrained_model_name)
     print(f"Custom model: {asr_model}")
 
 
