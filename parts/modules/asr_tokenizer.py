5a4acea3991c4a9014be78acda400d7c589e32ed
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-01 23:08:29 +0700

    #code

--- parts/modules/asr_tokenizer.py
+++ parts/modules/asr_tokenizer.py
@@ -1,7 +1,8 @@
 from transformers import AutoTokenizer
 from functools import cache
 
+
 @cache
 def get_tokenizer(model_name: str):
-  tokenizer = AutoTokenizer(model_name)
-  return tokenizer
+    tokenizer = AutoTokenizer(model_name)
+    return tokenizer
