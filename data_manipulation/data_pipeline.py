c268c5fab52875d6678fe2d1256638ee1f0d6c92
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-15 10:23:11 +0700

    #code

--- data_manipulation/data_pipeline.py
+++ data_manipulation/data_pipeline.py
@@ -7,16 +7,17 @@ import torchaudio.transforms as T
 import math
 import pandas as pd
 
+
 class DataProcessingPipeline:
     def __init__(self, conf: DictConfig):
         self.conf = OmegaConf.create(conf)
 
     def audio_transforms(self, sample_array):
         """
-            :param array: audio array get by torchaudio
-            :param params: params config in yaml file
-            :return: log mel spectrogram
-            """
+        :param array: audio array get by torchaudio
+        :param params: params config in yaml file
+        :return: log mel spectrogram
+        """
 
         # define mel spec transform function
         F_mel = T.MelSpectrogram(
@@ -37,8 +38,12 @@ class DataProcessingPipeline:
         mel_spectrogram = F_mel(sample_array)
 
         # log mel spectrogram
-        log_melspectrogram = F.amplitude_to_DB(mel_spectrogram, multiplier=10, amin=1e-10,
-                                               db_multiplier=math.log10(max(1e-10, 1)))
+        log_melspectrogram = F.amplitude_to_DB(
+            mel_spectrogram,
+            multiplier=10,
+            amin=1e-10,
+            db_multiplier=math.log10(max(1e-10, 1)),
+        )
 
         # adjust output
         # standard output: [bannks, n_frames (times)]
@@ -48,22 +53,26 @@ class DataProcessingPipeline:
 
     def add_noise2audio(self, sample_array: torch.Tensor, noise_array: torch.Tensor):
         """
-            :param sample_array: torch.Tensor,
-            :param noise_array
-            :return augmented audio with noise
-            """
+        :param sample_array: torch.Tensor,
+        :param noise_array
+        :return augmented audio with noise
+        """
         # work with noise have tensor([>= 1, ...n_frames]) 2 channels - audio with 2 channels can be considered as stereo sound
-        noise_array = noise_array[0, :sample_array.size(1)]  # take n_frames -> vector
+        noise_array = noise_array[0, : sample_array.size(1)]  # take n_frames -> vector
 
         noise_array = noise_array.unsqueeze(0)  # turn back to matrix reduce 1 channel
 
         # process noise_array
-        scaled_noise_arr = noise_array[:, :sample_array.size(1)]  # noise array must be tensor([1, ... n_frames])
+        scaled_noise_arr = noise_array[
+            :, : sample_array.size(1)
+        ]  # noise array must be tensor([1, ... n_frames])
 
         snr_dbs = torch.tensor([20, 10, 3])
 
         # augmented_audio
-        augmented_audio = F.add_noise(waveform=sample_array, noise=scaled_noise_arr, snr=snr_dbs)
+        augmented_audio = F.add_noise(
+            waveform=sample_array, noise=scaled_noise_arr, snr=snr_dbs
+        )
 
         return augmented_audio
 
@@ -72,12 +81,15 @@ class DataProcessingPipeline:
         params["window_fn"] = torch.hann_window  #
 
         #
-        PichShift_F = T.PitchShift(sample_rate=params["sample_rate"],
-                                   n_steps=params["pshift_steps"],
-                                   bins_per_octave=params["pshift_bins_per_octave"],
-                                   n_fft=params["n_fft"], win_length=params["win_length"],
-                                   hop_length=params["hop_length"],
-                                   window_fn=params["window_fn"])  #
+        PichShift_F = T.PitchShift(
+            sample_rate=params["sample_rate"],
+            n_steps=params["pshift_steps"],
+            bins_per_octave=params["pshift_bins_per_octave"],
+            n_fft=params["n_fft"],
+            win_length=params["win_length"],
+            hop_length=params["hop_length"],
+            window_fn=params["window_fn"],
+        )  #
 
         # get pitch shifted audio
         pshifted_audio = PichShift_F(sample_array)
@@ -86,15 +98,17 @@ class DataProcessingPipeline:
         return pshifted_audio
 
     def _trim_audio(audio_array, params):
-        """ Trim audio with Librosa
+        """Trim audio with Librosa
         :param audio_array
         :param params: configs in yaml file
         :return: trimmed audio array
         """
-        trimmed_audio, _ = librosa.effects.trim(y=audio_array,
-                                                top_db=params["top_db"],
-                                                frame_length=params["win_length"],
-                                                hop_length=params["hop_length"])
+        trimmed_audio, _ = librosa.effects.trim(
+            y=audio_array,
+            top_db=params["top_db"],
+            frame_length=params["win_length"],
+            hop_length=params["hop_length"],
+        )
 
         # return trimmed audio -> audio array
         return trimmed_audio
@@ -106,6 +120,12 @@ class DataProcessingPipeline:
     def _get_duration(self, path):
         duration = librosa.core.get_duration(path=path, sr=16000)
         return duration
+
+
+class LibriSpeechDataProcessingPipeline:
+    def __init__(self, conf: DictConfig):
+        super().__init__(DataProcessingPipeline)
+        self.conf = OmegaConf.create(conf)
 
     def prepare_ds2finetune(self, path):
         # process each sample
@@ -131,11 +151,6 @@ class DataProcessingPipeline:
             lower_transcripts.append(audio_transcript.lower())
         df["transcript"] = lower_transcripts
         df.to_csv("./metadata/ls/test-other.csv", index=False)
-
-class LibriSpeechDataProcessingPipeline:
-    def __init__(self, conf: DictConfig):
-        super().__init__(DataProcessingPipeline)
-        self.conf = OmegaConf.create(conf)
 
 
 class VNDataProcessingPipeline:
