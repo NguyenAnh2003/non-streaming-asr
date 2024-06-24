2e1e0d1442985c4c49af7cb2e36d5eb12241c72d
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-24 12:56:54 +0700

    ...

--- data_manipulation/data_pipeline.py
+++ data_manipulation/data_pipeline.py
@@ -145,56 +145,6 @@ class DataProcessingPipeline:
                     raise ValueError("Cannot convert str to float")
 
 
-class LibriSpeechDataProcessingPipeline:
-    def __init__(self, conf: DictConfig):
-        super().__init__(DataProcessingPipeline)
-        self.conf = OmegaConf.create(conf)
-
-    def prepare_ds_nemo(self,
-                        path,
-                        root_dir = "./librispeech/test-custom-other/",
-                        dest_dir = "../data_manipulation/librispeech/test-custom-other/",
-                        metadata_path = "./metadata/ls/test-other.csv"):
-        # process each sample
-        df = pd.read_csv(path)
-
-        # preprocess durations
-        durations = []
-        audio_paths = []
-        for audio in df["audio_id"]:
-            audio_path = root_dir + audio + ".flac"
-            a2 = dest_dir + audio + ".flac"
-            duration = self.get_duration(audio_path)
-            durations.append(duration)
-            audio_paths.append(a2)
-        df["duration"] = durations
-        df["path"] = audio_paths
-
-        # preprocess audio transcripts
-        lower_transcripts = []
-        for audio_transcript in df["transcript"]:
-            lower_transcripts.append(audio_transcript.lower())
-        df["transcript"] = lower_transcripts
-        df.to_csv(metadata_path, index=False)
-
-    def build_data_manifest(path: str, outpath: str):
-        reader = csv.reader(open(path, "r", encoding="utf-8"))
-        next(reader)
-        with open(outpath, 'w') as fout:
-            for line in reader:
-                try:
-                    duration = float(line[2])
-                    meta_data = {
-                        "audio_filepath": line[-1],
-                        "duration": duration,
-                        "text": line[1],
-                    }
-                    json.dump(meta_data, fout)
-                    fout.write("\n")
-                except ValueError:
-                    raise ValueError("Cannot convert str to float")
-
-
 class LibriSpeechDataProcessingPipeline(DataProcessingPipeline):
     def __init__(self, conf: DictConfig):
         super().__init__()
@@ -220,10 +170,10 @@ class VNDataProcessingPipeline(DataProcessingPipeline):
                         transript = text_file.read()
                         duration = self.get_duration(wav_path)
                         csv_writer.writerow([filename[:-4], wav_path, transript, duration])
-                        # print(f"id: {filename[:-4]} path: {wav_path} duration: {duration} transcript: {transript}")
-        print("DONE")
 
 if __name__ == "__main__":
     data_pipe = VNDataProcessingPipeline()
-    data_pipe.prepare_ds_csv(root_dir="/media/vkuai/NTFS_Data/nguyenanh_dataset/speech/vlsp_2020",
-                             dest_path="./metadata/vn/vlsp-2020-100h.csv")
+    # data_pipe.prepare_ds_csv(root_dir="/media/vkuai/NTFS_Data/nguyenanh_dataset/speech/vlsp_2020",
+    #                          dest_path="./metadata/vn/vlsp-2020-100h.csv")
+    data_pipe.build_metadata_manifests(path="./metadata/vn/vlsp-2020-100h.csv", outpath="./metadata/vn/manifests/vlsp-2020-100h-manifest.json")
+    print("DONE")
