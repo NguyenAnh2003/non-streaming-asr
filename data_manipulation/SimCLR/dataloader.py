e2f7f7f9d5bb794422f0d9fe9357486945c626d6
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-07-27 16:19:54 +0700

    #code

--- data_manipulation/SimCLR/dataloader.py
+++ data_manipulation/SimCLR/dataloader.py
@@ -6,34 +6,35 @@ import csv
 import pandas as pd
 import os
 
+
 class SimCLRDataset(Dataset):
     def __init__(self, conf: DictConfig = None, transforms=None) -> None:
         super().__init__()
+        # conf OmegaConf create
+        # transforms function for augment audio signal
         self.conf = OmegaConf.create(conf)
         self.transforms = transforms
         self.dataset = self._create_hf_dataset()
-        self.root_dir = self.conf.trainer.root_dir # setup root_dir
+        self.root_dir = self.conf.trainer.root_dir  # setup root dir
 
     def __getitem__(self, index):
         # input contain audio path
         # this transforms function will process with audio array
-        # then transforms to log melspec and aug this input
+        # then transforms to melspec and aug this input
         audio_path = self.dataset[index]["audio_filepath"]
         audio_path = os.path.join(self.root_dir, audio_path)
         transcript = self.dataset[index]["text"]
-        
+
         if self.transforms:
-            augmented_data = self.transforms(audio_path) # input (audio path)
+            augmented_data = self.transforms(audio_path)  # input (audio path)
 
         return augmented_data, transcript
 
     def _create_hf_dataset(self):
         # data path included in conf (OmegaConf)
+        # read json file
         dataset = HFDataset.from_json(self.conf.trainer.train_path)
         return dataset
-
-    def __len__(self):
-        return
 
 
 class SimCLRDataloader(DataLoader):
--- models/sim_clr.py
+++ models/sim_clr.py
@@ -10,6 +10,7 @@ from torch.optim import Adam, lr_scheduler
 from models.asr_model import ASRModel
 from transformers import AutoModel
 import torchaudio
+import numpy as np
 
 
 class BasicASRModel(nn.Module):
@@ -50,6 +51,42 @@ class BasicASRModel(nn.Module):
         return out
 
 
+class SpectrogramToDB(object):
+    """Turns a spectrogram from the power/amplitude scale to the decibel scale.
+    This output depends on the maximum value in the input spectrogram, and so
+    may return different values for an audio clip split into snippets vs. a
+    a full clip. This method is sourced from an earlier release of torchaudio and
+    is no longer present in current versions.
+    Args:
+        stype (str): scale of input spectrogram ("power" or "magnitude").  The
+            power being the elementwise square of the magnitude. default: "power"
+        top_db (float, optional): minimum negative cut-off in decibels.  A reasonable number
+            is 80.
+    """
+
+    def __init__(self, stype="power", top_db=None):
+        self.stype = stype
+        if top_db is not None and top_db < 0:
+            raise ValueError("top_db must be positive value")
+        self.top_db = top_db
+        self.multiplier = 10.0 if stype == "power" else 20.0
+        self.amin = 1e-10
+        self.ref_value = 1.0
+        self.db_multiplier = np.log10(np.maximum(self.amin, self.ref_value))
+
+    def __call__(self, spec):
+        # numerically stable implementation from librosa
+        # https://librosa.github.io/librosa/_modules/librosa/core/spectrum.html
+        spec_db = self.multiplier * torch.log10(torch.clamp(spec, min=self.amin))
+        spec_db -= self.multiplier * self.db_multiplier
+
+        if self.top_db is not None:
+            spec_db = torch.max(
+                spec_db, spec_db.new_full((1,), spec_db.max() - self.top_db)
+            )
+        return spec_db
+
+
 class AudioTransforms:
     # transform function contains
     # pitch shift, reverbration, adding noise
@@ -58,16 +95,28 @@ class AudioTransforms:
         self.rate = rate
 
         # transform function input should be mel spectrogram
-        self.transforms_waveform = [
+        self.augment_waveform = [
             self.pitch_shift,  # shifting audio pitch
             self.reverb_audio,  # reverb audio with room echo sound
         ]
 
-        self.transforms_melspectrogram = [self.time_mask, self.freq_mask]
+        self.augment_melspec = [self.time_mask, self.freq_mask]
 
     def transforms_wavform_2_melspec(self, audio_array):
-        mel_transform = T.MelSpectrogram()
+        # melspec transform function
+        mel_transform = T.MelSpectrogram(
+            sample_rate=self.rate,
+            n_mels=80,
+            n_fft=640,
+            win_length=640,
+            hop_length=321,
+            f_min=-80,
+            f_max=8000,
+            pad=0,
+        )
+
         mel_spec = mel_transform(audio_array)
+        mel_spec = SpectrogramToDB(stype="magnitude", top_db=8000)(mel_spec)
         return mel_spec
 
     def _load_audio_signal(self, audio_path):
@@ -77,16 +126,16 @@ class AudioTransforms:
     def audio_augment(self, audio_path):
         audio_array = self._load_audio_signal(audio_path)
         # random audio aug function
-        random_transform_wavform_func = random.choice(self.transforms_waveform)
-        audio_array = random_transform_wavform_func(audio_array)
+        for funcs in self.augment_waveform:
+            audio_array = funcs(audio_array)
 
         # convert to mel-spectrogram
-        log_mel_spec = self.transforms_wavform_2_melspec(audio_array)
-        # random aug log mel spectrogram
-        log_mel_spec_func = random.choice(self.transforms_melspectrogram)
-        log_mel_spec = log_mel_spec_func(log_mel_spec)
+        mel_spec = self.transforms_wavform_2_melspec(audio_array)
+        # random aug mel spectrogram
+        for funcs in self.augment_melspec:
+            mel_spec = funcs(mel_spec)
 
-        return log_mel_spec
+        return mel_spec
 
     def reverb_audio(self, audio_array):
         reverbered_audio = (
@@ -107,17 +156,50 @@ class AudioTransforms:
         )
         return pitch_shifted_audio
 
-    def time_mask(self):
-        pass
+    def time_mask(self, spec, T=40, num_masks=1, replace_with_zero=False):
+        cloned = spec.clone()
+        len_spectro = cloned.shape[2]
 
-    def freq_mask(self):
-        pass
+        for i in range(0, num_masks):
+            t = random.randrange(0, T)
+            t_zero = random.randrange(0, len_spectro - t)
+
+            # avoids randrange error if values are equal and range is empty
+            if t_zero == t_zero + t:
+                return cloned
+
+            mask_end = random.randrange(t_zero, t_zero + t)
+            if replace_with_zero:
+                cloned[0][:, t_zero:mask_end] = 0
+            else:
+                cloned[0][:, t_zero:mask_end] = cloned.mean()
+        return cloned
+
+    def freq_mask(self, spec, F=30, num_masks=1, replace_with_zero=False):
+        cloned = spec.clone()
+        num_mel_channels = cloned.shape[1]
+
+        for i in range(0, num_masks):
+            f = random.randrange(0, F)
+            f_zero = random.randrange(0, num_mel_channels - f)
+
+            # avoids randrange error if values are equal and range is empty
+            if f_zero == f_zero + f:
+                return cloned
+
+            mask_end = random.randrange(f_zero, f_zero + f)
+            if replace_with_zero:
+                cloned[0][f_zero:mask_end] = 0
+            else:
+                cloned[0][f_zero:mask_end] = cloned.mean()
+
+        return cloned
 
     def add_noise(self):
         pass
 
     def __call__(self, input):
-        # input is audio_path
+        # input is audio path
         x1 = self.audio_augment(input)
         x2 = self.audio_augment(input)
         return x1, x2
